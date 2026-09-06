eef50dd80a66971ff1aa5af1b0bf3b34c1e97df4
devansh choubey <devansh.choubey@kavida.ai>
2026-09-06 05:48:34 +0530

    avoid dynamic Python linker flags on macOS

--- setup_ci.py
+++ setup_ci.py
@@ -45,6 +45,7 @@ def _generate_path_with_gopath() -> str:
 class CustomBuildExt(build_ext):
     def build_extension(self, ext: Extension):
         bin_path = _generate_path_with_gopath()
+        dynamic_link = "False" if sys.platform == "darwin" else "True"
         go_env = json.loads(
             subprocess.check_output(["go", "env", "-json"]).decode("utf-8").strip()
         )
@@ -60,7 +61,7 @@ class CustomBuildExt(build_ext):
                 "gopy",
                 "build",
                 "-no-make",
-                "-dynamic-link=True",
+                f"-dynamic-link={dynamic_link}",
                 "-rename=True",
                 "-output",
                 destination,
